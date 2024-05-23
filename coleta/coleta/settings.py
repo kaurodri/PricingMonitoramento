8d2d41eb51b654bf649b10f0986e9be24a346004
HieA1lpha <engkauanrodrigues@gmail.com>
2024-05-23 18:42:51 -0300

    2024-05-23

--- coleta/coleta/settings.py
+++ coleta/coleta/settings.py
@@ -12,6 +12,7 @@ BOT_NAME = "coleta"
 SPIDER_MODULES = ["coleta.spiders"]
 NEWSPIDER_MODULE = "coleta.spiders"
 
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36 Edg/125.0.0.0'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = "coleta (+http://www.yourdomain.com)"
