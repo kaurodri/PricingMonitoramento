cb0785d89158b3a328dcccec511c05d8bb158afd
HieA1lpha <engkauanrodrigues@gmail.com>
2024-05-23 21:36:16 -0300

    2024-05-23

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # PricingMonitoramento
+
+para rodar o web scraping
+```bash
+scrapy crawl mercadolivre -o ../../data/data.jsonl
+```
--- src/coleta/settings.py
+++ src/coleta/settings.py
@@ -19,7 +19,7 @@ USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTM
 #USER_AGENT = "coleta (+http://www.yourdomain.com)"
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
