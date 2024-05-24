0a4663b3c18817e25dacbbca50963605695bdabe
HieA1lpha <engkauanrodrigues@gmail.com>
2024-05-23 21:23:01 -0300

    2024-05-23

--- src/coleta/spiders/mercadolivre.py
+++ src/coleta/spiders/mercadolivre.py
@@ -4,6 +4,8 @@ import scrapy
 class MercadolivreSpider(scrapy.Spider):
     name = "mercadolivre"
     start_urls = ["https://lista.mercadolivre.com.br/tenis-corrida-masculino"]
+    page_count = 1
+    max_pages = 10
 
     def parse(self, response):
         products = response.css('div.ui-search-result__content')
@@ -21,3 +23,9 @@ class MercadolivreSpider(scrapy.Spider):
                 'new_prince_reais': prices[1] if len(prices) > 0 else None,
                 'new_prince_centavos': cents[1] if len(prices) > 0 else None
             }
+
+            if self.page_count < self.max_pages:
+                next_page = response.css('li.andes-pagination__button.andes-pagination__button--next a::attr(href)').get()
+                if next_page:
+                    self.page_count += 1
+                    yield scrapy.Request(url=next_page, callback=self.parse)
